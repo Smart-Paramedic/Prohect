8b3e980f16d3a877d292427038240c112d504b7a
Smart-Paramedic <444122626@TVTC.EDU.SA>
2025-11-10 21:10:38 +0300

    test_db.php

--- test_db.php
+++ test_db.php
@@ -1,10 +1,8 @@
 <?php
-  include 'db.php';
-
-  $result = mysqli_query($conn, "SELECT * FROM medical_cases");
-
-  while ($row = mysqli_fetch_assoc($result)) {
-    echo $row['case_name'] . "<br>";
-  }
+include 'db.php';
+$result = mysqli_query($conn, "SELECT * FROM medical_cases");
+while ($row = mysqli_fetch_assoc($result)) {
+    echo $row['case_name_ar'] . "<br>";
+}
 ?>
 
